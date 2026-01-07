3389ed650a20bbaf2c758beced8d19f86d938632
sidscri <sidscri@yahoo.com>
2026-01-07 13:45:17 -0600

    deleted Project v2.0 folder Kept Project-v2

--- KenpoFlashcardsProject v2.0/gradlew.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-set "APP_HOME=%~dp0"
-set "JAVA_EXE=java"
-if not "%JAVA_HOME%"=="" if exist "%JAVA_HOME%\bin\java.exe" set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
-"%JAVA_EXE%" -classpath "%APP_HOME%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
-exit /b %ERRORLEVEL%
